8847136e81df2fdf7e6392b51bceb50e4ac7be11
ChandanaAasrithaPentakota <chandanaaasritha@gmail.com>
2026-02-10 14:08:15 +0530

    graph.py, config.py and groq_llm.py modified, requirements added

---
+++ llms/groq_llm.py
@@ -0,0 +1,25 @@
+from config import GROQ_API_KEY
+from langchain_groq import ChatGroq
+
+
+def get_groq_llm(model_name: str = "openai/gpt-oss-120b",temperature: float = 0.2):
+    """
+    Returns an initialized Groq LLM instance.
+
+    This function should be used by all agents
+    to ensure consistent LLM configuration.
+    """
+
+
+    if not GROQ_API_KEY:
+        raise ValueError(
+            "GROQ_API_KEY not found. Please set it in your .env file."
+        )
+
+    llm = ChatGroq(
+        model=model_name,
+        api_key=GROQ_API_KEY,
+        temperature=temperature,
+    )
+
+    return llm

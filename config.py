eee16470d813da36fc4470803fa352ce25da0056
ChandanaAasrithaPentakota <chandanaaasritha@gmail.com>
2026-02-13 16:19:35 +0530

    rag medical added

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+HF_TOKEN=os.getenv("HF_TOKEN")
+OLLAMA_API_KEY=os.getenv("OLLAMA_API_KEY")
+PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
